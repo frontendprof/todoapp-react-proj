a744bc4ae0faca8d8600b9e406715d4e038d5a39
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-24 07:47:52 -0500

    Preventing default behavior of button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:"",
+        key:''
+      }
+    }
+  }
+
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    
+  }
+
   render(){
     return(
       <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Enter text"/>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}/>
             <button type="submit">Add</button>
           </form>
         </header>
